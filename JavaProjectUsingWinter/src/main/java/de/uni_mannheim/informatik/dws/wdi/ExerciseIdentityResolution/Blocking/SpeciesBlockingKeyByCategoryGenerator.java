aff2b701d2bc7c67111836e6ec1517d8ca9ce0c1
unknown <tgalla@mail.uni-mannheim.de>
2021-11-26 18:48:03 +0100

    added SpeciesBlockingKeyGenerato - not working yet

---
+++ JavaProjectUsingWinter/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Blocking/SpeciesBlockingKeyByCategoryGenerator.java
@@ -0,0 +1,25 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Species;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.BlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.RecordBlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.Pair;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.DataIterator;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class SpeciesBlockingKeyByCategoryGenerator extends
+        RecordBlockingKeyGenerator<Species, Attribute> {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Override
+    public void generateBlockingKeys(Species record, Processable<Correspondence<Attribute, Matchable>> correspondences,
+                                     DataIterator<Pair<String, Species>> resultCollector) {
+
+    }
+
+}
